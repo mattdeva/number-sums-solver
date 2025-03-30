6fa2253445bc5b9752f4b9aa1d220ba4e649ed80
mattdeva <matt.neel.10@gmail.com>
2025-03-30 00:15:48 -0400

    hint update

--- number_sums_solver/components/solver.py
+++ number_sums_solver/components/solver.py
@@ -16,7 +16,7 @@ class Solver:
         return len(self.integers)
 
     @property
-    def addens_sum_list(self) -> dict[Sequence, int]: # being created each time not great but should be fine...
+    def addens_sum_list(self) -> list[tuple[Sequence, int]]: # being created each time not great but should be fine...
         out_list = []
         for i in range(1, len(self)+1): # loop through number of integers
             combinations = itertools.combinations(self.integers, i) # create combinations of i
@@ -34,9 +34,3 @@ class Solver:
     def get_addens(self, sum_:int) -> list[int]:
         return _flatten_and_unique(self.squeeze(sum_))
     
-# Solver([1,2,2,3,5,6]).squeeze(4)
-# Solver([1,1,1,1]).squeeze(4)
-# Solver([2]).squeeze(2)
-# Solver([1,1,1]).squeeze(2)
-
-# square
