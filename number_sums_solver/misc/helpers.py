228f25e2aac09197687b013dc1958926dc808d33
mattdeva <matt.neel.10@gmail.com>
2025-05-25 13:45:43 -0400

    :bug:

--- number_sums_solver/misc/helpers.py
+++ number_sums_solver/misc/helpers.py
@@ -2,13 +2,13 @@
 
 import matplotlib.pyplot as plt
 
-def mark_point_on_image(image, coord_dict, color='red'):
+def mark_point_on_image(image, coordinate_list:list[tuple[int]], color='red'):
     """  """
     # dont want to modify origional
     image_copy = image.copy()
 
     # Display the image
     plt.imshow(image_copy, cmap='gray' if len(image.shape) == 2 else None)
-    for coordinates, int_ in coord_dict.items():
+    for coordinates in coordinate_list:
         plt.scatter(coordinates[0], coordinates[1], color=color, marker='x', s=100)  # 'x' marker for the point
     plt.show()
